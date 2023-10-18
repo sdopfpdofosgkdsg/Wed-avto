8e42da268da834773b0867f03efbaa4fb08d323e
sdopfpdofosgkdsg <144285204+sdopfpdofosgkdsg@users.noreply.github.com>
2023-10-18 19:12:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+// Слайд-шоу
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function myFunc(n) {
+  showSlides(slideIndex += n);
+}
+
+function forSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+// Аккордеон 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+  
+    this.classList.toggle("activee");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
